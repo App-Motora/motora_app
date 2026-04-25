19dc17ab6e30e92ea68855298929b8bab8743d47
Marina Sabino <marisabinocarvalho@gmail.com>
2026-04-25 11:46:45 -0300

    Tranforma o HomePage em StatefullWidget

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,9 +3,14 @@ import 'package:motora_app/components/activity_card.dart';
 import 'package:motora_app/components/float_button.dart';
 import 'package:motora_app/components/header.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(

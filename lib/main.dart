04ba6ed39f2199a2424c372263cd7a9c07b242e1
Marina Sabino <marisabinocarvalho@gmail.com>
2026-04-25 12:03:24 -0300

    Mudando o app.dart para um arquivo separado do main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:motora_app/home_page.dart';
+import 'package:motora_app/app.dart';
 
 void main() {
-  runApp(MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: HomePage()
-      ),
-    );
-  }
+  runApp(const App());
 }
